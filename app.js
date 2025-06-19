a74d998af7fc60c567e069024c548d5428ad46dc
Yagnik Shingala <143250874+yagnikshingala123@users.noreply.github.com>
2025-06-19 20:04:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:5173',
-      'https://atv-frontend-two.vercel.app'
+      'https://atv-frontend-alpha.vercel.app'
     ],
     credentials: true,
   })
